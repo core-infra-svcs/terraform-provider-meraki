74f7b10420c16f5ca256aa4a7c08c43bb31a9738
Dexter Park <ddexterpark@icloud.com>
2022-11-15 02:55:07 +0900

    fix: deprecated string value references

--- internal/provider/organization_resource.go
+++ internal/provider/organization_resource.go
@@ -2,10 +2,8 @@ package provider
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	apiclient "github.com/core-infra-svcs/dashboard-api-go/client"
-
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -29,12 +27,15 @@ type OrganizationResource struct {
 
 // OrganizationResourceModel describes the resource data model.
 type OrganizationResourceModel struct {
-	Api_enabled     types.Bool   `tfsdk:"api_enabled"`
-	Cloud_region    types.String `tfsdk:"cloud_region"`
-	Id              types.String `tfsdk:"id"`
-	Licensing_model types.String `tfsdk:"licensing_model"`
-	Name            types.String `tfsdk:"name"`
-	Url             types.String `tfsdk:"url"`
+	ApiEnabled             types.Bool   `tfsdk:"api_enabled"`
+	CloudRegion            types.String `tfsdk:"cloud_region"`
+	ManagementDetailsName  types.String `tfsdk:"management_details_name"`
+	ManagementDetailsValue types.String `tfsdk:"management_details_value"`
+	Id                     types.String `tfsdk:"id"`
+	OrgId                  types.String `tfsdk:"organization_id"`
+	LicensingModel         types.String `tfsdk:"licensing_model"`
+	Name                   types.String `tfsdk:"name"`
+	Url                    types.String `tfsdk:"url"`
 }
 
 func (r *OrganizationResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -45,10 +46,7 @@ func (r *OrganizationResource) GetSchema(ctx context.Context) (tfsdk.Schema, dia
 	return tfsdk.Schema{
 		// This description is used by the documentation generator and the language server.
 		MarkdownDescription: "Organization resource - Manage the organizations that the user has privileges on",
-
 		Attributes: map[string]tfsdk.Attribute{
-
-			// TODO - As a developer I must manually inspect the required/optional status of each attribute
 			"api_enabled": {
 				Description:         "Enable API access",
 				MarkdownDescription: "API-specific settings",
@@ -76,7 +74,30 @@ func (r *OrganizationResource) GetSchema(ctx context.Context) (tfsdk.Schema, dia
 				PlanModifiers:       nil,
 			},
 			"id": {
-				Description:         "Organization ID",
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+				Type: types.StringType,
+			},
+			"organization_id": {
+				Description:         "Organization Id",
+				MarkdownDescription: "The Id of the organization",
+				Type:                types.StringType,
+				Required:            false,
+				Optional:            true,
+				Computed:            true,
+				Sensitive:           false,
+				Attributes:          nil,
+				DeprecationMessage:  "",
+				Validators:          nil,
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					resource.UseStateForUnknown(),
+				},
+			},
+			"licensing_model": {
+				Description:         "Organization licensing model. Can be 'co-term', 'per-device', or 'subscription'.",
 				MarkdownDescription: "",
 				Type:                types.StringType,
 				Required:            false,
@@ -88,9 +109,22 @@ func (r *OrganizationResource) GetSchema(ctx context.Context) (tfsdk.Schema, dia
 				Validators:          nil,
 				PlanModifiers:       nil,
 			},
-			"licensing_model": {
-				Description:         "Organization licensing model. Can be 'co-term', 'per-device', or 'subscription'.",
-				MarkdownDescription: "",
+			"management_details_name": {
+				Description:         "The name of the organization's management system",
+				MarkdownDescription: "The name of the organization's management system",
+				Type:                types.StringType,
+				Required:            false,
+				Optional:            true,
+				Computed:            true,
+				Sensitive:           false,
+				Attributes:          nil,
+				DeprecationMessage:  "",
+				Validators:          nil,
+				PlanModifiers:       nil,
+			},
+			"management_details_value": {
+				Description:         "Information about the organization's management system",
+				MarkdownDescription: "Information about the organization's management system",
 				Type:                types.StringType,
 				Required:            false,
 				Optional:            true,
@@ -161,9 +195,20 @@ func (r *OrganizationResource) Create(ctx context.Context, req resource.CreateRe
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	createOrganization := *apiclient.NewInlineObject165(data.Name.Value)
+	createOrganization := *apiclient.NewInlineObject165(data.Name.ValueString())
+
+	// Set Management Details
+	var name = data.ManagementDetailsName.ValueString()
+	var value = data.ManagementDetailsValue.ValueString()
+
+	detail := apiclient.OrganizationsManagementDetails{
+		Name:  &name,
+		Value: &value,
+	}
+	details := []apiclient.OrganizationsManagementDetails{detail}
+	organizationsManagement := apiclient.OrganizationsManagement{Details: details}
+	createOrganization.SetManagement(organizationsManagement)
+
 	response, d, err := r.client.OrganizationsApi.CreateOrganization(context.Background()).CreateOrganization(createOrganization).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
@@ -174,28 +219,41 @@ func (r *OrganizationResource) Create(ctx context.Context, req resource.CreateRe
 			"-- Response Header --",
 			fmt.Sprintf("%v\n", d.Header),
 		)
-		resp.Diagnostics.AddError(
-			"-- Request URL --",
-			fmt.Sprintf("%v\n", d.Request.URL),
-		)
 		return
 	}
 
-	// extract map string interface data into struct
-	responseData, _ := json.Marshal(response)
-	var results apiclient.InlineResponse20068
-	json.Unmarshal(responseData, &results)
-
 	// save into the Terraform state.
-	data.Id = types.String{Value: results.GetId()}
-	data.Name = types.String{Value: results.GetName()}
-	data.Cloud_region = types.String{Value: results.Cloud.Region.GetName()}
-	data.Url = types.String{Value: results.GetUrl()}
-	data.Api_enabled = types.Bool{Value: results.Api.GetEnabled()}
-	data.Licensing_model = types.String{Value: results.Licensing.GetModel()}
+	data.OrgId = types.StringValue(response.GetId())
+	data.Name = types.StringValue(response.GetName())
+	data.CloudRegion = types.StringValue(response.Cloud.Region.GetName())
+	data.Url = types.StringValue(response.GetUrl())
+	data.ApiEnabled = types.BoolValue(response.Api.GetEnabled())
+	data.LicensingModel = types.StringValue(response.Licensing.GetModel())
+
+	// Management Details Response
+	if len(response.Management.Details) > 0 {
+
+		responseDetails := response.Management.GetDetails()
+
+		// name attribute
+		if managementDetailName := responseDetails[0].GetName(); responseDetails[0].HasName() == true {
+			data.ManagementDetailsName = types.StringValue(managementDetailName)
+		} else {
+			data.ManagementDetailsName = types.StringNull()
+		}
+
+		// Value attribute
+		if managementDetailValue := responseDetails[0].GetValue(); responseDetails[0].HasValue() == true {
+			data.ManagementDetailsValue = types.StringValue(managementDetailValue)
+		} else {
+			data.ManagementDetailsValue = types.StringNull()
+		}
+	} else {
+		data.ManagementDetailsName = types.StringNull()
+		data.ManagementDetailsValue = types.StringNull()
+	}
 
 	// Write logs using the tflog package
-	// Documentation: https://terraform.io/plugin/log
 	tflog.Trace(ctx, "create resource")
 
 	// Save data into Terraform state
@@ -212,9 +270,7 @@ func (r *OrganizationResource) Read(ctx context.Context, req resource.ReadReques
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	response, d, err := r.client.OrganizationsApi.GetOrganization(context.Background(), data.Id.Value).Execute()
+	response, d, err := r.client.OrganizationsApi.GetOrganization(context.Background(), data.OrgId.ValueString()).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"-- Read Error --",
@@ -224,19 +280,39 @@ func (r *OrganizationResource) Read(ctx context.Context, req resource.ReadReques
 			"-- Response Header --",
 			fmt.Sprintf("%v\n", d.Header),
 		)
-		resp.Diagnostics.AddError(
-			"-- Request URL --",
-			fmt.Sprintf("%v\n", d.Request.URL),
-		)
 		return
 	}
 
-	data.Id = types.String{Value: response.GetId()}
-	data.Name = types.String{Value: response.GetName()}
-	data.Cloud_region = types.String{Value: response.Cloud.Region.GetName()}
-	data.Url = types.String{Value: response.GetUrl()}
-	data.Api_enabled = types.Bool{Value: response.Api.GetEnabled()}
-	data.Licensing_model = types.String{Value: response.Licensing.GetModel()}
+	// save into the Terraform state.
+	data.OrgId = types.StringValue(response.GetId())
+	data.Name = types.StringValue(response.GetName())
+	data.CloudRegion = types.StringValue(response.Cloud.Region.GetName())
+	data.Url = types.StringValue(response.GetUrl())
+	data.ApiEnabled = types.BoolValue(response.Api.GetEnabled())
+	data.LicensingModel = types.StringValue(response.Licensing.GetModel())
+
+	// Management Details Response
+	if len(response.Management.Details) > 0 {
+
+		responseDetails := response.Management.GetDetails()
+
+		// name attribute
+		if managementDetailName := responseDetails[0].GetName(); responseDetails[0].HasName() == true {
+			data.ManagementDetailsName = types.StringValue(managementDetailName)
+		} else {
+			data.ManagementDetailsName = types.StringNull()
+		}
+
+		// Value attribute
+		if managementDetailValue := responseDetails[0].GetValue(); responseDetails[0].HasValue() == true {
+			data.ManagementDetailsValue = types.StringValue(managementDetailValue)
+		} else {
+			data.ManagementDetailsValue = types.StringNull()
+		}
+	} else {
+		data.ManagementDetailsName = types.StringNull()
+		data.ManagementDetailsValue = types.StringNull()
+	}
 
 	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -248,25 +324,38 @@ func (r *OrganizationResource) Update(ctx context.Context, req resource.UpdateRe
 	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 
+	// Check for required parameters
+	if len(data.OrgId.ValueString()) < 1 {
+		resp.Diagnostics.AddError("Missing organizationId", fmt.Sprintf("%s", data.OrgId.ValueString()))
+	}
+
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-
 	// create payload
 	updateOrganization := apiclient.NewInlineObject166()
-	updateOrganization.SetName(data.Name.Value)
+	updateOrganization.SetName(data.Name.ValueString())
 
-	// nested params are strongly typed
-	var enabled apiclient.OrganizationsOrganizationIdApi
-	enabled.SetEnabled(data.Api_enabled.Value)
-	updateOrganization.SetApi(enabled)
+	// API Enabled
+	var enabled = data.ApiEnabled.ValueBool()
+	Api := apiclient.OrganizationsOrganizationIdApi{Enabled: &enabled}
+	updateOrganization.SetApi(Api)
+
+	// Set Management Details
+	var name = data.ManagementDetailsName.ValueString()
+	var value = data.ManagementDetailsValue.ValueString()
+	detail := apiclient.OrganizationsManagementDetails{
+		Name:  &name,
+		Value: &value,
+	}
+	details := []apiclient.OrganizationsManagementDetails{detail}
+	organizationsManagement := apiclient.OrganizationsManagement{Details: details}
+	updateOrganization.SetManagement(organizationsManagement)
 
 	// Initialize provider client
 	response, d, err := r.client.OrganizationsApi.UpdateOrganization(context.Background(),
-		data.Id.Value).UpdateOrganization(*updateOrganization).Execute()
+		data.OrgId.ValueString()).UpdateOrganization(*updateOrganization).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"-- Update Error --",
@@ -276,25 +365,39 @@ func (r *OrganizationResource) Update(ctx context.Context, req resource.UpdateRe
 			"-- Response Header --",
 			fmt.Sprintf("%v\n", d.Header),
 		)
-		resp.Diagnostics.AddError(
-			"-- Request URL --",
-			fmt.Sprintf("%v\n", d.Request.URL),
-		)
 		return
 	}
 
-	// extract map string interface data into struct
-	responseData, _ := json.Marshal(response)
-	var results apiclient.InlineResponse20068
-	json.Unmarshal(responseData, &results)
-
 	// save into the Terraform state.
-	data.Id = types.String{Value: results.GetId()}
-	data.Name = types.String{Value: results.GetName()}
-	data.Cloud_region = types.String{Value: results.Cloud.Region.GetName()}
-	data.Url = types.String{Value: results.GetUrl()}
-	data.Api_enabled = types.Bool{Value: results.Api.GetEnabled()}
-	data.Licensing_model = types.String{Value: results.Licensing.GetModel()}
+	data.OrgId = types.StringValue(response.GetId())
+	data.Name = types.StringValue(response.GetName())
+	data.CloudRegion = types.StringValue(response.Cloud.Region.GetName())
+	data.Url = types.StringValue(response.GetUrl())
+	data.ApiEnabled = types.BoolValue(response.Api.GetEnabled())
+	data.LicensingModel = types.StringValue(response.Licensing.GetModel())
+
+	// Management Details Response
+	if len(response.Management.Details) > 0 {
+
+		responseDetails := response.Management.GetDetails()
+
+		// name attribute
+		if managementDetailName := responseDetails[0].GetName(); responseDetails[0].HasName() == true {
+			data.ManagementDetailsName = types.StringValue(managementDetailName)
+		} else {
+			data.ManagementDetailsName = types.StringNull()
+		}
+
+		// Value attribute
+		if managementDetailValue := responseDetails[0].GetValue(); responseDetails[0].HasValue() == true {
+			data.ManagementDetailsValue = types.StringValue(managementDetailValue)
+		} else {
+			data.ManagementDetailsValue = types.StringNull()
+		}
+	} else {
+		data.ManagementDetailsName = types.StringNull()
+		data.ManagementDetailsValue = types.StringNull()
+	}
 
 	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -310,24 +413,32 @@ func (r *OrganizationResource) Delete(ctx context.Context, req resource.DeleteRe
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
 	// Delete Organization
-	response, err := r.client.OrganizationsApi.DeleteOrganization(context.Background(), data.Id.Value).Execute()
+	response, err := r.client.OrganizationsApi.DeleteOrganization(context.Background(), data.OrgId.ValueString()).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"-- Delete Error --",
 			fmt.Sprintf("%v\n", err.Error()),
 		)
 		resp.Diagnostics.AddError(
-			"-- Response Header --",
-			fmt.Sprintf("%v\n", response.Header),
-		)
-		resp.Diagnostics.AddError(
-			"-- Request URL --",
-			fmt.Sprintf("%v\n", response.Request.URL),
+			"-- Response --",
+			fmt.Sprintf("%v\n", response),
 		)
 		return
+	}
+
+	if response.StatusCode != 204 {
+		resp.Diagnostics.AddError(
+			"-- Delete Error --",
+			fmt.Sprintf("%v\n", err.Error()),
+		)
+		resp.Diagnostics.AddError(
+			"-- Response --",
+			fmt.Sprintf("%v\n", response),
+		)
+		return
+	} else {
+		resp.State.RemoveResource(ctx)
 	}
 
 }
--- internal/provider/organization_resource_test.go
+++ internal/provider/organization_resource_test.go
@@ -2,13 +2,13 @@ package provider
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccOrganizationResource(t *testing.T) {
-	var id string
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
@@ -21,32 +21,29 @@ func TestAccOrganizationResource(t *testing.T) {
 					resource.TestCheckResourceAttr("meraki_organization.test", "name", "testOrg1"),
 					resource.TestCheckResourceAttr("meraki_organization.test", "api_enabled", "true"),
 
-					// save postgres Id to variable
-					resource.TestCheckResourceAttrWith("meraki_organization.test", "id",
-						func(value string) error {
-							id = value
-							if len(id) < 1 {
-								return fmt.Errorf("failed to save postgresId from state: %s", id)
-							}
-							return nil
-						}),
+					/*
+						// save postgres Id to variable
+							resource.TestCheckResourceAttrWith("meraki_organization.test", "organization_id",
+								func(value string) error {
+									fmt.Println(fmt.Sprintf("ENV VAR: %s", os.Getenv("TF_MERAKI_DASHBOARD_ORG_ID")))
+									fmt.Println(fmt.Sprintf("Value: %s", value))
+									return nil
+								}),
+					*/
 				),
-				//
-
 			},
 
-			/*
-				// TODO - Figure out why update testing produces a 404 from the Meraki API. May require a custom test check.
-					// Update testing
-					{
-						Config: testAccOrganizationResourceUpdate(id),
-						Check: resource.ComposeAggregateTestCheckFunc(
-							resource.TestCheckResourceAttr("meraki_organization.test", "name", "testOrg1"),
-							resource.TestCheckResourceAttr("meraki_organization.test", "api_enabled", "true"),
-							resource.TestCheckResourceAttr("meraki_organization.test", "id", id),
-						),
-					},
-			*/
+			// Update testing
+			{
+				// PreConfig: func() { testOrgIdExistsPreCheck(t) },
+				Config: testAccOrganizationResourceUpdate(testAccOrganizationResourceConfigUpdate),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					//resource.TestCheckResourceAttr("meraki_organization.test", "organization_id", os.Getenv("TF_MERAKI_DASHBOARD_ORG_ID")),
+					resource.TestCheckResourceAttr("meraki_organization.test", "name", "testOrg2"),
+					resource.TestCheckResourceAttr("meraki_organization.test", "api_enabled", "true"),
+					resource.TestCheckResourceAttr("meraki_organization.test", "management_details_name", "MSP ID"),
+				),
+			},
 
 			// Delete testing automatically occurs in TestCase
 		},
@@ -60,15 +57,30 @@ resource "meraki_organization" "test" {
 }
 `
 
-/*
+const testAccOrganizationResourceConfigUpdate = `
+resource "meraki_organization" "test" {
+	name = "testOrg2"
+	api_enabled = true
+	management_details_name = "MSP ID"
+	management_details_value = "123456"
+}
+`
+
 func testAccOrganizationResourceUpdate(postgresId string) string {
 	return fmt.Sprintf(`
 resource "meraki_organization" "test" {
-	id = "%s"
-	name = "testOrg1"
+	organization_id = "%s"
+	name = "testOrg2"
 	api_enabled = true
+	management_details_name = "MSP ID"
+	management_details_value = "123456"
 
 }
 `, postgresId)
 }
-*/
+
+func testOrgIdExistsPreCheck(t *testing.T) {
+	if v := os.Getenv("TF_MERAKI_DASHBOARD_ORG_ID"); v == "" {
+		t.Fatal("TF_MERAKI_DASHBOARD_ORG_ID must be set for acceptance tests")
+	}
+}
