82b310eff1022272b30db6db47bd3dfdefda0cd7
Dexter Park <ddexterpark@icloud.com>
2022-12-07 05:01:21 +0900

    cosmetic: formatting
    - updated elements of struct to CamelCase
    - updated elements of struct hints & schema to snake_case
    -  removed TODO's / Debug Print Statements

--- internal/provider/organizations_saml_idps_datasource.go
+++ internal/provider/organizations_saml_idps_datasource.go
@@ -32,14 +32,10 @@ type OrganizationsSamlIdpsDataSourceModel struct {
 
 // OrganizationsSamlIdpDataSourceModel describes the data source data model.
 type OrganizationsSamlIdpDataSourceModel struct {
-	// Check that each item is typed correctly and the names match the tfsdk.Schema attributes.
-	// Check that all names are in SnakeCase
-	// Tip: Usual "types.Object" should be modified.
-
-	Consumerurl             types.String `tfsdk:"consumer_url"`
-	Idpid                   types.String `tfsdk:"idp_id"`
-	Slologouturl            types.String `tfsdk:"slo_logout_url"`
-	X509certsha1fingerprint types.String `tfsdk:"x_509cert_sha1_fingerprint"`
+	ConsumerUrl             types.String `tfsdk:"consumer_url"`
+	IdpId                   types.String `tfsdk:"idp_id"`
+	SloLogOutUrl            types.String `tfsdk:"slo_logout_url"`
+	X509CertSha1FingerPrint types.String `tfsdk:"x_509_cert_sha1_fingerprint"`
 }
 
 func (d *OrganizationsSamlIdpsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -63,12 +59,6 @@ func (d *OrganizationsSamlIdpsDataSource) GetSchema(ctx context.Context) (tfsdk.
 				Validators:          nil,
 				PlanModifiers:       nil,
 			},
-
-			// TODO - Inspect each Attribute and ensure each matches the API endpoint.
-			// TODO - Check the attributes MarkdownDescription.
-			// TODO - Ensure either Required OR Optional + Computed is set.
-			// TODO - If the value is a password or token set the Sensitive flag to true.
-
 			"organization_id": {
 				Description:         "Organization ID",
 				MarkdownDescription: "Organization ID",
@@ -82,14 +72,11 @@ func (d *OrganizationsSamlIdpsDataSource) GetSchema(ctx context.Context) (tfsdk.
 				Validators:          nil,
 				PlanModifiers:       nil,
 			},
-
 			"list": {
 				Description:         "List of Saml IDPs",
 				MarkdownDescription: "List of Saml IDPs",
 				Optional:            true,
-				//Type:                types.SetType{ElemType: types.StringType},
 				Attributes: tfsdk.SetNestedAttributes(map[string]tfsdk.Attribute{
-
 					"consumer_url": {
 						Description:         "URL that is consuming SAML Identity Provider (IdP)",
 						MarkdownDescription: "URL that is consuming SAML Identity Provider (IdP)",
@@ -116,7 +103,6 @@ func (d *OrganizationsSamlIdpsDataSource) GetSchema(ctx context.Context) (tfsdk.
 						Validators:          nil,
 						PlanModifiers:       nil,
 					},
-
 					"slo_logout_url": {
 						Description:         "Dashboard will redirect users to this URL when they sign out.",
 						MarkdownDescription: "Dashboard will redirect users to this URL when they sign out.",
@@ -130,7 +116,7 @@ func (d *OrganizationsSamlIdpsDataSource) GetSchema(ctx context.Context) (tfsdk.
 						Validators:          nil,
 						PlanModifiers:       nil,
 					},
-					"x_509cert_sha1_fingerprint": {
+					"x_509_cert_sha1_fingerprint": {
 						Description:         "Fingerprint (SHA1) of the SAML certificate provided by your Identity Provider (IdP). This will be used for encryption / validation.",
 						MarkdownDescription: "Fingerprint (SHA1) of the SAML certificate provided by your Identity Provider (IdP). This will be used for encryption / validation.",
 						Type:                types.StringType,
@@ -179,7 +165,6 @@ func (d *OrganizationsSamlIdpsDataSource) Read(ctx context.Context, req datasour
 	}
 
 	// Initialize provider client and make API call
-	//inlineResp, httpResp, err := d.client.SamlApi.GetOrganizationSamlIdps(context.Background(), data.OrganizationId.ValueString()).Execute()
 	inlineResp, httpResp, err := d.client.OrganizationsApi.GetOrganizationSamlIdps(context.Background(), data.OrganizationId.ValueString()).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
@@ -206,23 +191,18 @@ func (d *OrganizationsSamlIdpsDataSource) Read(ctx context.Context, req datasour
 
 	// save inlineResp data into Terraform state.
 	data.Id = types.StringValue("example-id")
-	//data.OrganizationId = types.StringValue("1239794")
-
-	//resp.Diagnostics.AddError("Inline repsonse is : ", fmt.Sprintf("%s %s %s %s", inlineResp[0].GetIdpId(), inlineResp[0].GetConsumerUrl(), inlineResp[0].GetSloLogoutUrl(), inlineResp[0].GetX509certSha1Fingerprint()))
-	//resp.Diagnostics.AddError("Inline repsonse is : ", fmt.Sprintf("IDP ID IS %s", inlineResp[0].GetIdpId()))
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	for _, saml_idp := range inlineResp {
+	for _, samlIdp := range inlineResp {
 		var result OrganizationsSamlIdpDataSourceModel
 
-		result.Idpid = types.StringValue(saml_idp.GetIdpId())
-		result.Consumerurl = types.StringValue(saml_idp.GetConsumerUrl())
-		result.Slologouturl = types.StringValue(saml_idp.GetSloLogoutUrl())
-		result.X509certsha1fingerprint = types.StringValue(saml_idp.GetX509certSha1Fingerprint())
-		//fmt.Println(saml_idp)
+		result.IdpId = types.StringValue(samlIdp.GetIdpId())
+		result.ConsumerUrl = types.StringValue(samlIdp.GetConsumerUrl())
+		result.SloLogOutUrl = types.StringValue(samlIdp.GetSloLogoutUrl())
+		result.X509CertSha1FingerPrint = types.StringValue(samlIdp.GetX509certSha1Fingerprint())
 		data.List = append(data.List, result)
 	}
 
