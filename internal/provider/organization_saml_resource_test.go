3e467bd1081bb0c81f081b06ca2dd3dfea9b940f
Dexter Park <ddexterpark@icloud.com>
2022-12-07 08:37:08 +0900

    feat: organization SAML Enabled
    Created organization SAML resource dependency

---
+++ internal/provider/organization_saml_resource_test.go
@@ -0,0 +1,53 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccOrganizationSamlResource(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+
+			// Create test organization
+			{
+				Config: testAccOrganizationSamlResourceConfigCreateOrg,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("meraki_organization.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("meraki_organization.test", "name", "test_meraki_organization_saml"),
+				),
+			},
+
+			// Enable SAML on organization test
+			{
+				Config: testAccOrganizationSamlResourceConfigSaml,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("meraki_organization_saml.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("meraki_organization_saml.test", "enabled", "true"),
+				),
+			},
+
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+const testAccOrganizationSamlResourceConfigCreateOrg = `
+ resource "meraki_organization" "test" {
+ 	name = "test_meraki_organization_saml"
+ 	api_enabled = true
+ }
+ `
+
+const testAccOrganizationSamlResourceConfigSaml = `
+resource "meraki_organization" "test" {
+}
+
+resource "meraki_organization_saml" "test" {
+	organization_id = resource.meraki_organization.test.organization_id
+	enabled = true
+}
+`
