36caf46e48dcc36afebff8cd9f49c7b7ece1e18b
Dexter Park <ddexterpark@icloud.com>
2022-12-07 11:03:13 +0900

    refactor: TestAccOrganizationsSamlIdpsDataSource
    - updated test steps to be more atomic
    - added data source back to provider list (merge conflict)

--- internal/provider/organizations_saml_idps_datasource_test.go
+++ internal/provider/organizations_saml_idps_datasource_test.go
@@ -13,38 +13,33 @@ func TestAccOrganizationsSamlIdpsDataSource(t *testing.T) {
 
 			// Create test organization
 			{
-				Config: testAccOrganizationsSamlIdpsDataSourceConfigCreateOrg,
+				Config: testAccOrganizationsSamlIdpsResourceConfigCreateOrg,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("meraki_organization.test", "id", "example-id"),
 					resource.TestCheckResourceAttr("meraki_organization.test", "name", "test_meraki_organizations_saml_idps"),
 				),
 			},
 
-			/*
-				// TODO - Create test admin
-					{
-						Config: testAccOrganizationsSamlIdpsDataSourceConfigCreateAdmin,
-						Check: resource.ComposeAggregateTestCheckFunc(
-							//resource.TestCheckResourceAttr("meraki_organization_admin", "name", "test_admin"),
-							//resource.TestCheckResourceAttr("meraki_organization_admin", "email", "test_admin@example.com"),
-						),
-					},
-			*/
+			// Enable SAML on organization test
+			{
+				Config: testAccOrganizationsSamlIdpsResourceConfigSaml,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("meraki_organization_saml.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("meraki_organization_saml.test", "enabled", "true"),
+				),
+			},
 
-			/*
-					// TODO - Create test saml idp
-				 			{
-				 				Config: testAccOrganizationsSamlIdpsDataSourceConfigCreateSamlIdp,
-				 				Check: resource.ComposeAggregateTestCheckFunc(
-				 					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "id", "example-id"),
-				 					// resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "organization_id", ""),
-				 					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "slo_logout_url", "https://sbuxforyou.com"),
-				 					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "x_509cert_sha1_fingerprint", "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:99"),
-				 				),
-				 			},
-			*/
+			// Create and Read Idp test
+			{
+				Config: testAccOrganizationsSamlIdpsResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "slo_logout_url", "https://sbuxforyou.com"),
+					resource.TestCheckResourceAttr("meraki_organizations_saml_idp.test", "x_509_cert_sha1_fingerprint", "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:24"),
+				),
+			},
 
-			// TODO - Read testing
+			// Read Idps testing
 			{
 				Config: testAccOrganizationsSamlIdpsDataSourceConfigRead,
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -52,8 +47,8 @@ func TestAccOrganizationsSamlIdpsDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "id", "example-id"),
 
 					resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.#", "1"),
-					//resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.x_509_cert_sha1_fingerprint", "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:99"),
-					//resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.slo_logout_url", "https://somewhereelseforyou.com"),
+					resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.x_509_cert_sha1_fingerprint", "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:24"),
+					resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.slo_logout_url", "https://sbuxforyou.com"),
 
 					//resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.idp_id", ""),
 					//resource.TestCheckResourceAttr("data.meraki_organizations_saml_idps.test", "list.0.consumer_url", ""),
@@ -63,34 +58,31 @@ func TestAccOrganizationsSamlIdpsDataSource(t *testing.T) {
 	})
 }
 
-const testAccOrganizationsSamlIdpsDataSourceConfigCreateOrg = `
-resource "meraki_organization" "test" {
-	name = "test_meraki_organizations_saml_idps"
-	api_enabled = true
-}
-`
-
-/* TODO - Create admin to complete test coverage
-const testAccOrganizationsSamlIdpsDataSourceConfigCreateAdmin = `
-resource "meraki_organization_admin" "test" {
-	name = "test_admin"
-	email = "test_admin@example.com"
-}
-`
-*/
-
-/* TODO -Enable SAML to complete test coverage
-const testAccOrganizationsSamlIdpsDataSourceConfigCreateSamlIdp = `
-	resource "meraki_organization" "test" {}
-
-	resource "meraki_organizations_saml_idp" "test" {
- 	organization_id = resource.meraki_organization.test.organization_id
- 	slo_logout_url = "https://sbuxforyou.com"
- 	x_509cert_sha1_fingerprint = "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:99"
+const testAccOrganizationsSamlIdpsResourceConfigCreateOrg = `
+ resource "meraki_organization" "test" {
+ 	name = "test_meraki_organizations_saml_idps"
+ 	api_enabled = true
  }
  `
-*/
 
+const testAccOrganizationsSamlIdpsResourceConfigSaml = `
+resource "meraki_organization" "test" {
+}
+resource "meraki_organization_saml" "test" {
+	organization_id = resource.meraki_organization.test.organization_id
+	enabled = true
+}
+`
+
+const testAccOrganizationsSamlIdpsResourceConfig = `
+resource "meraki_organization" "test" {
+}
+ resource "meraki_organizations_saml_idp" "test" {
+	organization_id = resource.meraki_organization.test.organization_id
+	slo_logout_url = "https://sbuxforyou.com"
+	x_509_cert_sha1_fingerprint = "00:11:22:33:44:55:66:77:88:99:00:11:22:33:44:55:66:77:88:24"
+}
+`
 const testAccOrganizationsSamlIdpsDataSourceConfigRead = `
 resource "meraki_organization" "test" {}
 
