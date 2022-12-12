53adc42e6f37959d145783999804b5d8db13a67e
Dexter Park <ddexterpark@icloud.com>
2022-12-12 16:39:25 +0900

    Update provider.go

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -102,6 +102,7 @@ func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.R
 		NewOrganizationsSamlIdpResource,
 		NewOrganizationsAdminResource,
 		NewOrganizationsAdaptivePolicyAclResource,
+		NewDevicesResource,
 	}
 }
 
