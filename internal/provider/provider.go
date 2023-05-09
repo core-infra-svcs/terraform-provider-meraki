9fbc98b4ee05cf1c551366eae89145db0baaad55
Suvani Pulla (CW) <spulla@spulla38876.starbucks.net>
2023-05-09 17:37:19 -0400

    Add resource into provider list

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -172,6 +172,7 @@ func (p *CiscoMerakiProvider) Resources(ctx context.Context) []func() resource.R
 		NewNetworksSyslogServersResource,
 		NewNetworksApplianceVlansSettingsResource,
 		NewNetworksApplianceSettingsResource,
+		NewNetworksSwitchAccessPoliciesResource,
 	}
 }
 
