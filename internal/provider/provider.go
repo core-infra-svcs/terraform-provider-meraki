7b1d6f68c1b22442cc2fc73834ed1e51fb59a1a3
Suvani Pulla (CW) <spulla@spulla38876.starbucks.net>
2023-05-11 08:11:15 -0400

    Add storm control data srouce

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -180,6 +180,7 @@ func (p *CiscoMerakiProvider) DataSources(ctx context.Context) []func() datasour
 		NewOrganizationsSamlIdpsDataSource,
 		NewOrganizationsAdaptivePolicyAclsDataSource,
 		NewOrganizationsSamlRolesDataSource,
+		NewNetworksSwitchStormcontrolDataSource,
 	}
 }
 
