a9cf5e6be7a6a777836d4be8538442a5c93fb6db
kikumar552 <kiran.surapathi552@gmail.com>
2023-07-03 19:50:46 -0700

    refactoring tests to add devices

--- internal/provider/networks_appliance_ports_datasource_test.go
+++ internal/provider/networks_appliance_ports_datasource_test.go
@@ -1,6 +1,8 @@
 package provider
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -11,14 +13,6 @@ func TestAccNetworkAppliancePortsDatasource(t *testing.T) {
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-
-			// Create test Organization
-			{
-				Config: testAccNetworkAppliancePortsDatasourceConfigCreateOrganization,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_organization.test", "name", "test_acc_meraki_networks_appliance_ports"),
-				),
-			},
 
 			// TODO - ImportState testing - This only works when hard-coded networkId.
 			/*
@@ -32,73 +26,74 @@ func TestAccNetworkAppliancePortsDatasource(t *testing.T) {
 
 			// Create and Read Network .
 			{
-				Config: testAccNetworkAppliancePortsDatasourceConfigCreateNetwork,
+				Config: testAccNetworkAppliancePortsDatasourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID")),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("meraki_network.test", "name", "Main Office"),
 					resource.TestCheckResourceAttr("meraki_network.test", "timezone", "America/Los_Angeles"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.#", "1"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.0", "tag1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "3"),
+					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "1"),
 					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "appliance"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.1", "switch"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.2", "wireless"),
 					resource.TestCheckResourceAttr("meraki_network.test", "notes", "Additional description of the network"),
 				),
 			},
 
-			/*
-				//TODO To List Network Appliance Ports VLANs enabled Network Needed for Testing.
-				//  List Network Appliance Ports.
-				{
-					Config: testAccNetworkAppliancePortsDatasourceConfigListNetworkAppliancePorts,
-					Check:  resource.ComposeAggregateTestCheckFunc(
-
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "access_policy", "access_policy"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "allowed_vlans", "allowed_vlans"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "drop_untagged_traffic", "true"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "enabled", "true"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "number", "4"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "type", "access"),
-						resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "vlan", "12345"),
-
-					),
-				},
-			*/
-
+			//TODO To List Network Appliance Ports VLANs enabled Network Needed for Testing.
+			//  List Network Appliance Ports.
+			{
+				Config: testAccNetworkAppliancePortsDatasourceConfigListNetworkAppliancePorts(os.Getenv("TF_ACC_MERAKI_MX_SERIAL"), os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID")),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.#", "3"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.allowed_vlans", "all"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.drop_untagged_traffic", "true"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.enabled", "true"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.number", "3"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.type", "trunk"),
+					resource.TestCheckResourceAttr("data.meraki_networks_appliance_ports.test", "list.0.vlan", "0"),
+				),
+			},
 		},
 	})
 }
 
-const testAccNetworkAppliancePortsDatasourceConfigCreateOrganization = `
- resource "meraki_organization" "test" {
- 	name = "test_acc_meraki_networks_appliance_ports"
- 	api_enabled = true
- } 
- `
-
-const testAccNetworkAppliancePortsDatasourceConfigCreateNetwork = `
- resource "meraki_organization" "test" {}
- resource "meraki_network" "test" {
-	depends_on = [resource.meraki_organization.test]
-	organization_id = resource.meraki_organization.test.organization_id
-	product_types = ["appliance", "switch", "wireless"]
+func testAccNetworkAppliancePortsDatasourceConfigCreateNetwork(orgId string) string {
+	result := fmt.Sprintf(`
+ resource "meraki_network" "test" {	
+	organization_id = "%s"
+	product_types = ["appliance"]
 	tags = ["tag1"]
 	name = "Main Office"
 	timezone = "America/Los_Angeles"
 	notes = "Additional description of the network"
 }
- `
+ `, orgId)
+	return result
+}
 
-const testAccNetworkAppliancePortsDatasourceConfigListNetworkAppliancePorts = `
-resource "meraki_organization" "test" {}
-resource "meraki_network" "test" {
-	depends_on = [resource.meraki_organization.test]	
-	product_types = ["appliance", "switch", "wireless"]	
+// testAccNetworkAppliancePortsDatasourceConfigListNetworkAppliancePorts is a constant string that defines the configuration for reading a networks_appliance_ports datasource in your tests.
+// It depends on both the organization and network resources.
+func testAccNetworkAppliancePortsDatasourceConfigListNetworkAppliancePorts(serial string, orgId string) string {
+	result := fmt.Sprintf(`
+	resource "meraki_network" "test" {
+		organization_id = "%s"
+		product_types = ["appliance"]			
+	}
+	resource "meraki_networks_devices_claim" "test" {
+    depends_on = [resource.meraki_network.test]
+    network_id = resource.meraki_network.test.network_id
+    serials = [
+      "%s"
+    ]
+    }
+	resource "meraki_networks_appliance_vlans_settings" "test" {
+		depends_on = [resource.meraki_network.test, meraki_networks_devices_claim.test]
+		network_id = resource.meraki_network.test.network_id
+		vlans_enabled = true
+	}  
+    data "meraki_networks_appliance_ports" "test" {
+	depends_on = [resource.meraki_network.test, meraki_networks_devices_claim.test, meraki_networks_appliance_vlans_settings.test]	
+	network_id = resource.meraki_network.test.network_id	
+    }	
+`, orgId, serial)
+	return result
 }
-data "meraki_networks_appliance_ports" "test" {
-	  depends_on = [resource.meraki_organization.test,
-	  resource.meraki_network.test]	
-      network_id = resource.meraki_network.test.network_id
-	  
-}
-`
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -177,6 +177,7 @@ func (p *CiscoMerakiProvider) Resources(ctx context.Context) []func() resource.R
 		NewNetworksSwitchDscpToCosMappingsResource,
 		NewNetworksSwitchMtuResource,
 		NewNetworksGroupPolicyResource,
+		NewNetworksDevicesClaimResource,
 	}
 }
 
@@ -190,6 +191,7 @@ func (p *CiscoMerakiProvider) DataSources(ctx context.Context) []func() datasour
 		NewOrganizationsAdaptivePolicyAclsDataSource,
 		NewOrganizationsSamlRolesDataSource,
 		NewNetworkGroupPoliciesDataSource,
+		NewNetworksAppliancePortsDataSource,
 	}
 }
 
