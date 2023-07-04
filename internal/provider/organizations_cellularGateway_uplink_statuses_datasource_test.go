5e54c79efd2878f893ff0c9d16257cf4cae5dd3d
kikumar552 <kiran.surapathi552@gmail.com>
2023-07-04 00:23:41 -0700

    adding device dependency to acceptance tests

--- internal/provider/organizations_cellularGateway_uplink_statuses_datasource_test.go
+++ internal/provider/organizations_cellularGateway_uplink_statuses_datasource_test.go
@@ -21,35 +21,23 @@ func TestAccOrganizationsCellularGatewayUplinkStatusesDataSource(t *testing.T) {
 		// Steps is a slice of TestStep where each TestStep represents a test case.
 		Steps: []resource.TestStep{
 
-			// Create and Read an Organization.
-			{
-				Config: testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateOrganization,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_organization.test", "id", "example-id"),
-					resource.TestCheckResourceAttr("meraki_organization.test", "name", "test_meraki_organizations_cellular_gateway_uplink_statuses"),
-				),
-			},
-
 			// Create and Read a Network.
 			{
-				Config: testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateNetwork,
+				Config: testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID")),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("meraki_network.test", "name", "Main Office"),
 					resource.TestCheckResourceAttr("meraki_network.test", "timezone", "America/Los_Angeles"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.#", "1"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.0", "tag1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "4"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "appliance"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.1", "cellularGateway"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.2", "switch"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.3", "wireless"),
+					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "1"),
+					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "cellularGateway"),
 					resource.TestCheckResourceAttr("meraki_network.test", "notes", "Additional description of the network"),
 				),
 			},
 
 			// Claim and Read NetworksDevicesClaim
 			{
-				Config: testAccNetworksDevicesClaimResourceConfigCreate(os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
+				Config: testAccNetworksDevicesClaimResourceConfigCreate(os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID"), os.Getenv("TF_ACC_MERAKI_MG_SERIAL")),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("meraki_networks_devices_claim.test", "id", "example-id"),
 				),
@@ -57,11 +45,11 @@ func TestAccOrganizationsCellularGatewayUplinkStatusesDataSource(t *testing.T) {
 
 			// Read OrganizationsCellularGatewayUplinkStatuses
 			{
-				Config: testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigRead(os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
+				Config: testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigRead(os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID"), os.Getenv("TF_ACC_MERAKI_MG_SERIAL"), os.Getenv("TF_ACC_MERAKI_ORGANZIATION_ID")),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.meraki_organizations_cellular_gateway_uplink_statuses.test", "id", "example-id"),
 					resource.TestCheckResourceAttr("data.meraki_organizations_cellular_gateway_uplink_statuses.test", "list.#", "1"),
-					resource.TestCheckResourceAttr("data.meraki_organizations_cellular_gateway_uplink_statuses.test", "list.0.serial", os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
+					resource.TestCheckResourceAttr("data.meraki_organizations_cellular_gateway_uplink_statuses.test", "list.0.serial", os.Getenv("TF_ACC_MERAKI_MG_SERIAL")),
 					resource.TestCheckResourceAttr("data.meraki_organizations_cellular_gateway_uplink_statuses.test", "list.0.model", "MG21-NA"),
 				),
 			},
@@ -79,59 +67,57 @@ const testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateOrg
 
 // testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateNetwork is a constant string that defines the configuration for creating a network resource in your tests.
 // It depends on the organization resource.
-const testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateNetwork = `
-resource "meraki_organization" "test" {}
-
-resource "meraki_network" "test" {
-	depends_on = [resource.meraki_organization.test]
-	organization_id = resource.meraki_organization.test.organization_id
-	product_types = ["appliance", "switch", "wireless", "cellularGateway"]
+func testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigCreateNetwork(orgId string) string {
+	result := fmt.Sprintf(`
+resource "meraki_network" "test" {	
+	organization_id = "%s"
+	product_types = ["cellularGateway"]
 	tags = ["tag1"]
 	name = "Main Office"
 	timezone = "America/Los_Angeles"
 	notes = "Additional description of the network"
 }
-`
+`, orgId)
+	return result
+}
 
 // testAccNetworksDevicesClaimResourceConfigCreate is a constant string that defines the configuration for creating and reading a networks_devices_claim resource in your tests.
 // It depends on both the organization and network resources.
-func testAccNetworksDevicesClaimResourceConfigCreate(serial string) string {
+func testAccNetworksDevicesClaimResourceConfigCreate(orgId string, serial string) string {
 	result := fmt.Sprintf(`
-resource "meraki_organization" "test" {}
 resource "meraki_network" "test" {
-        depends_on = [resource.meraki_organization.test]
-        product_types = ["appliance", "switch", "wireless", "cellularGateway"]
+	    organization_id = "%s"
+        product_types = ["cellularGateway"]
 }    
 resource "meraki_networks_devices_claim" "test" {
-    depends_on = [resource.meraki_network.test, resource.meraki_organization.test]
+    depends_on = [resource.meraki_network.test]
     network_id = resource.meraki_network.test.network_id
     serials = [
       "%s"
   ]
 }	
-`, serial)
+`, orgId, serial)
 	return result
 }
 
 // testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigRead is a constant string that defines the configuration for creating and updating a organizations_cellularGateway_uplink_statuses resource in your tests.
 // It depends on both the organization and network resources.
-func testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigRead(serial string) string {
+func testAccOrganizationsCellularGatewayUplinkStatusesDataSourceConfigRead(orgId string, serial string, orgId2 string) string {
 	result := fmt.Sprintf(`
-resource "meraki_organization" "test" {}
 resource "meraki_network" "test" {
-        depends_on = [resource.meraki_organization.test]
-        product_types = ["appliance", "switch", "wireless", "cellularGateway"]
+	    organization_id = "%s"
+        product_types = ["cellularGateway"]
 }    
 resource "meraki_networks_devices_claim" "test" {
-    depends_on = [resource.meraki_network.test, resource.meraki_organization.test]
+    depends_on = [resource.meraki_network.test]
     network_id = resource.meraki_network.test.network_id
     serials = [
       "%s"
   ]
 }
 data "meraki_organizations_cellular_gateway_uplink_statuses" "test" {
-	organization_id = resource.meraki_organization.test.organization_id
+	organization_id = "%s"
 }	
-`, serial)
+`, orgId, serial, orgId2)
 	return result
 }
