e0035b03ca128f22f53771c2b533bff9dc1f6ff8
kikumar552 <kiran.surapathi552@gmail.com>
2024-02-24 15:07:17 -0800

    adding l3 firewall rules datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -338,6 +338,7 @@ func (p *CiscoMerakiProvider) DataSources(ctx context.Context) []func() datasour
 		NewNetworksSwitchQosRulesDataSource,
 		NewNetworksApplianceVpnSiteToSiteVpnDatasource,
 		NewNetworksSwitchMtuDataSource,
+                NewNetworksApplianceFirewallL3FirewallRulesDataSource,
 	}
 }
 
