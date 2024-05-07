650f41c2098e58382429ed6bd9557deeb82c846d
kikumar <kiran.surapathi552@gmail.com>
2024-05-06 18:59:48 -0700

    adding storm control datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -375,6 +375,7 @@ func (p *CiscoMerakiProvider) DataSources(ctx context.Context) []func() datasour
 		NewNetworksSwitchMtuDataSource,
 		NewDevicesManagementInterfaceDatasource,
 		NewNetworksApplianceFirewallL3FirewallRulesDataSource,
+                NetworksSwitchStormControlDataSource,
 	}
 }
 
