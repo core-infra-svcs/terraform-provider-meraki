61cbaf45f0e7949603e0126712e63b99bda1a1ec
Dexter Park <iamdexterpark@icloud.com>
2024-07-10 03:02:22 +0900

    feat: device claim enhancements
    - device claim resource is more resilient to various claim conditions and is verified to claim and update a list of serials without issue.

--- internal/provider/networks_devices_claim_resource.go
+++ internal/provider/networks_devices_claim_resource.go
@@ -2,20 +2,21 @@ package provider
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-	"net/http"
-	"strings"
-	"time"
-
-	"github.com/core-infra-svcs/terraform-provider-meraki/internal/provider/jsontypes"
 	"github.com/core-infra-svcs/terraform-provider-meraki/tools"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	openApiClient "github.com/meraki/dashboard-api-go/client"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 )
 
 var (
@@ -33,9 +34,9 @@ type NetworksDevicesClaimResource struct {
 }
 
 type NetworksDevicesClaimResourceModel struct {
-	Id        jsontypes.String   `tfsdk:"id"`
-	NetworkId jsontypes.String   `tfsdk:"network_id"`
-	Serials   []jsontypes.String `tfsdk:"serials"`
+	Id        types.String `tfsdk:"id"`
+	NetworkId types.String `tfsdk:"network_id"`
+	Serials   types.Set    `tfsdk:"serials"`
 }
 
 func (r *NetworksDevicesClaimResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -47,8 +48,7 @@ func (r *NetworksDevicesClaimResource) Schema(ctx context.Context, req resource.
 		MarkdownDescription: "Claim devices into a network",
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
-				Computed:   true,
-				CustomType: jsontypes.StringType,
+				Computed: true,
 			},
 			"network_id": schema.StringAttribute{
 				MarkdownDescription: "Network Id",
@@ -56,12 +56,10 @@ func (r *NetworksDevicesClaimResource) Schema(ctx context.Context, req resource.
 				Validators: []validator.String{
 					stringvalidator.LengthBetween(1, 31),
 				},
-				CustomType: jsontypes.StringType,
 			},
 			"serials": schema.SetAttribute{
 				MarkdownDescription: "The serials of the devices that should be claimed",
-				ElementType:         jsontypes.StringType,
-				CustomType:          jsontypes.SetType[jsontypes.String](),
+				ElementType:         types.StringType,
 				Computed:            true,
 				Optional:            true,
 			},
@@ -93,51 +91,104 @@ func (r *NetworksDevicesClaimResource) Create(ctx context.Context, req resource.
 		return
 	}
 
-	var serials []string
-	for _, serial := range data.Serials {
-		serials = append(serials, serial.ValueString())
+	var serialsToClaim []string
+	var serialsUnclaimed []string
+	var serialsAlreadyClaimed []string
+	var claimedSerials []string
+
+	for _, serial := range data.Serials.Elements() {
+		serialsToClaim = append(serialsToClaim, strings.Trim(serial.String(), "\""))
 	}
 
-	claimNetworkDevices := *openApiClient.NewClaimNetworkDevicesRequest(serials)
-
-	maxRetries := r.client.GetConfig().MaximumRetries
-	retryDelay := r.client.GetConfig().Retry4xxErrorWaitTime
-
-	httpResp, err := r.client.NetworksApi.ClaimNetworkDevices(ctx, data.NetworkId.ValueString()).ClaimNetworkDevicesRequest(claimNetworkDevices).Execute()
-	retries := 0
-	remaining := maxRetries - retries
-	for retries < maxRetries && httpResp != nil && httpResp.StatusCode == http.StatusBadRequest {
-		tflog.Warn(ctx, "Retrying Create API call", map[string]interface{}{
-			"maxRetries":        maxRetries,
-			"retryDelay":        retryDelay,
-			"remainingAttempts": remaining,
-			"httpStatusCode":    httpResp.StatusCode,
-		})
-		time.Sleep(time.Duration(retryDelay) * time.Second)
-		httpResp, err = r.client.NetworksApi.ClaimNetworkDevices(ctx, data.NetworkId.ValueString()).ClaimNetworkDevicesRequest(claimNetworkDevices).Execute()
-		retries++
-	}
-
+	// Get current devices in the network to check if they are already claimed
+	currentDevicesResp, _, err := r.client.DevicesApi.GetNetworkDevices(ctx, data.NetworkId.ValueString()).Execute()
 	if err != nil {
-		resp.Diagnostics.AddError("Error claiming devices", err.Error())
-		if httpResp != nil {
-			resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
+		resp.Diagnostics.AddError("Error fetching current network devices", fmt.Sprintf("Could not fetch current network devices: %s", err))
+		return
+	}
+
+	// Update list of verified claimed devices
+	for _, device := range currentDevicesResp {
+		serial, ok := device["serial"].(string)
+		if ok {
+			serialsAlreadyClaimed = append(serialsAlreadyClaimed, serial)
 		}
-		return
 	}
 
-	if httpResp.StatusCode != 200 {
-		resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("%v", httpResp.StatusCode))
-		return
+	// Convert claimed devices list into a map for quick lookup
+	existsInSerialsAlreadyClaimed := make(map[string]bool)
+	for _, item := range serialsAlreadyClaimed {
+		existsInSerialsAlreadyClaimed[item] = true
 	}
 
-	data.Id = jsontypes.StringValue(data.NetworkId.ValueString())
+	// Determine unclaimed serials
+	for _, serial := range serialsToClaim {
+		if !existsInSerialsAlreadyClaimed[serial] {
+			serialsUnclaimed = append(serialsUnclaimed, serial)
+		}
+	}
+
+	if len(serialsUnclaimed) > 0 {
+		claimNetworkDevices := *openApiClient.NewClaimNetworkDevicesRequest(serialsUnclaimed)
+
+		maxRetries := r.client.GetConfig().MaximumRetries
+		retryDelay := time.Duration(r.client.GetConfig().Retry4xxErrorWaitTime)
+
+		apiCall := func() (interface{}, *http.Response, error) {
+			time.Sleep(retryDelay * time.Millisecond)
+			httpResp, err := r.client.NetworksApi.ClaimNetworkDevices(ctx, data.NetworkId.ValueString()).ClaimNetworkDevicesRequest(claimNetworkDevices).Execute()
+			return httpResp.Body, httpResp, err
+		}
+
+		claimDevicesResp, httpResp, err := tools.CustomHttpRequestRetry(ctx, maxRetries, retryDelay, apiCall)
+		if err != nil {
+			handleError(ctx, err, httpResp, "Error claiming devices", resp)
+			return
+		}
+
+		if httpResp.StatusCode != 200 {
+			resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("Received status code: %v", httpResp.StatusCode))
+			return
+		}
+
+		// Decode the response from claiming devices
+		var claimResponse map[string]interface{}
+		if err := json.NewDecoder(claimDevicesResp.(io.Reader)).Decode(&claimResponse); err != nil {
+			resp.Diagnostics.AddError("Error decoding response", fmt.Sprintf("Error decoding response: %v", err))
+			return
+		}
+
+		if serials, ok := claimResponse["serials"].([]interface{}); ok {
+			for _, serial := range serials {
+				if s, ok := serial.(string); ok {
+					claimedSerials = append(claimedSerials, s)
+				}
+			}
+		}
+	} else {
+		tflog.Info(ctx, "All devices are already claimed in the network", map[string]interface{}{
+			"network_id": data.NetworkId.ValueString(),
+		})
+	}
+
+	// Convert the claimed serials list into types.StringList
+	claimedSerialsList, claimedSerialsListErr := types.SetValueFrom(ctx, types.StringType, claimedSerials)
+	if claimedSerialsListErr.HasError() {
+		resp.Diagnostics.Append(claimedSerialsListErr...)
+		return
+	}
+	data.Serials = claimedSerialsList
+
+	data.Id = data.NetworkId
+
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	tflog.Trace(ctx, "Created resource")
+	tflog.Trace(ctx, "Created resource", map[string]interface{}{
+		"network_id": data.NetworkId.ValueString(),
+	})
 }
 
 func (r *NetworksDevicesClaimResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -147,38 +198,48 @@ func (r *NetworksDevicesClaimResource) Read(ctx context.Context, req resource.Re
 		return
 	}
 
-	inlineResp, httpResp, err := r.client.DevicesApi.GetNetworkDevices(ctx, data.NetworkId.ValueString()).Execute()
+	// Call the API to get network devices
+	devicesResp, httpResp, err := r.client.DevicesApi.GetNetworkDevices(ctx, data.NetworkId.ValueString()).Execute()
 	if err != nil {
-		resp.Diagnostics.AddError("Error reading devices", err.Error())
-		if httpResp != nil {
-			resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
-		}
+		resp.Diagnostics.AddError("Failed to read list of serials", err.Error())
 		return
 	}
 
+	// Verify HTTP status code
 	if httpResp.StatusCode != 200 {
-		resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("%v", httpResp.StatusCode))
+		resp.Diagnostics.AddError("Unexpected HTTP response status code", fmt.Sprintf("Received status code: %d", httpResp.StatusCode))
 		return
 	}
 
-	var respSerials []jsontypes.String
-	for _, device := range inlineResp {
-		if serial, sOk := device["serial"].(string); sOk {
-			respSerials = append(respSerials, jsontypes.StringValue(serial))
+	// Process the response and collect serial numbers
+	var respSerials []string
+	for _, device := range devicesResp {
+		if serial, ok := device["serial"].(string); ok {
+			respSerials = append(respSerials, serial)
 		}
 	}
 
-	data.Serials = respSerials
-	data.Id = jsontypes.StringValue(data.NetworkId.ValueString())
+	// Convert serial numbers to a type that can be stored in the Terraform state
+	serialsList, serialsListErr := types.SetValueFrom(ctx, types.StringType, respSerials)
+	if serialsListErr != nil {
+		resp.Diagnostics.AddError("Failed to convert serial numbers for Terraform state", fmt.Sprintf("%s", serialsListErr))
+		return
+	}
+	data.Serials = serialsList
 
+	// Set the read data ID and store the state
+	data.Id = types.StringValue(data.NetworkId.ValueString())
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	tflog.Trace(ctx, "Read resource")
+	tflog.Trace(ctx, "Successfully read network device resource", map[string]interface{}{
+		"network_id": data.NetworkId.ValueString(),
+	})
 }
 
+// Update method
 func (r *NetworksDevicesClaimResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var data, state NetworksDevicesClaimResourceModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
@@ -187,57 +248,50 @@ func (r *NetworksDevicesClaimResource) Update(ctx context.Context, req resource.
 		return
 	}
 
-	var planSerials, stateSerials, serialsToAdd, serialsToRemove []string
-	for _, serial := range data.Serials {
-		planSerials = append(planSerials, serial.ValueString())
-	}
-	for _, serial := range state.Serials {
-		stateSerials = append(stateSerials, serial.ValueString())
-	}
+	// Extract serials from the plan and state
+	planSerials := extractSerials(data.Serials)
+	stateSerials := extractSerials(state.Serials)
 
-	serialsToAdd = difference(planSerials, stateSerials)
-	serialsToRemove = difference(stateSerials, planSerials)
+	// Determine which serials to add or remove
+	serialsToAdd := difference(planSerials, stateSerials)
+	serialsToRemove := difference(stateSerials, planSerials)
 
-	claimNetworkDevices := *openApiClient.NewClaimNetworkDevicesRequest(serialsToAdd)
-	httpResp, err := r.client.NetworksApi.ClaimNetworkDevices(ctx, data.NetworkId.ValueString()).ClaimNetworkDevicesRequest(claimNetworkDevices).Execute()
-	if err != nil {
-		resp.Diagnostics.AddError("Error claiming devices", err.Error())
-		if httpResp != nil {
-			resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
-		}
-		return
-	}
-
-	if httpResp.StatusCode != 200 {
-		resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("%v", httpResp.StatusCode))
-		return
-	}
-
-	for _, serial := range serialsToRemove {
-		removeNetworkDevices := *openApiClient.NewRemoveNetworkDevicesRequest(strings.Trim(serial, "\""))
-		httpResp, err := r.client.NetworksApi.RemoveNetworkDevices(ctx, data.NetworkId.ValueString()).RemoveNetworkDevicesRequest(removeNetworkDevices).Execute()
-		if err != nil {
-			resp.Diagnostics.AddError("Error removing devices", err.Error())
-			if httpResp != nil {
-				resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
-			}
-			return
-		}
-
-		if httpResp.StatusCode != 204 {
-			resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("%v", httpResp.StatusCode))
+	// Claim new devices
+	if len(serialsToAdd) > 0 {
+		if err := manageDeviceClaims(ctx, r.client, data.NetworkId.ValueString(), serialsToAdd, true, resp); err != nil {
 			return
 		}
 	}
 
-	data.Id = jsontypes.StringValue(data.NetworkId.ValueString())
+	// Remove devices
+	if len(serialsToRemove) > 0 {
+		if err := manageDeviceClaims(ctx, r.client, data.NetworkId.ValueString(), serialsToRemove, false, resp); err != nil {
+			return
+		}
+	}
 
+	// Ensure the state is updated correctly with the new serials
+	claimedSerials := mergeSerials(planSerials, serialsToAdd)
+
+	updatedSerialsList, serialsListErr := types.SetValueFrom(ctx, types.StringType, claimedSerials)
+	if serialsListErr != nil {
+		resp.Diagnostics.AddError("Failed to convert serial numbers for Terraform state", fmt.Sprintf("%s", serialsListErr))
+		return
+	}
+	data.Serials = updatedSerialsList
+
+	// Update the ID and state
+	data.Id = types.StringValue(data.NetworkId.ValueString())
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	tflog.Trace(ctx, "Updated resource")
+	tflog.Trace(ctx, "Updated network devices resource", map[string]interface{}{
+		"network_id": data.NetworkId.ValueString(),
+		"added":      serialsToAdd,
+		"removed":    serialsToRemove,
+	})
 }
 
 func (r *NetworksDevicesClaimResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
@@ -247,29 +301,149 @@ func (r *NetworksDevicesClaimResource) Delete(ctx context.Context, req resource.
 		return
 	}
 
-	for _, serial := range data.Serials {
-		removeNetworkDevices := *openApiClient.NewRemoveNetworkDevicesRequest(strings.Trim(serial.ValueString(), "\""))
-		httpResp, err := r.client.NetworksApi.RemoveNetworkDevices(ctx, data.NetworkId.ValueString()).RemoveNetworkDevicesRequest(removeNetworkDevices).Execute()
-		if err != nil {
-			resp.Diagnostics.AddError("Error removing devices", err.Error())
-			if httpResp != nil {
-				resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
-			}
-			return
-		}
+	// Extract serials from the state
+	serials := extractSerials(data.Serials)
 
-		if httpResp.StatusCode != 204 {
-			resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("%v", httpResp.StatusCode))
+	if len(serials) > 0 {
+		// Attempt to remove all devices
+		if err := removeDevices(ctx, r.client, data.NetworkId.ValueString(), serials, resp); err != nil {
 			return
 		}
 	}
 
+	// Confirm removal of the resource from state
 	resp.State.RemoveResource(ctx)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	tflog.Trace(ctx, "Deleted resource")
+	tflog.Trace(ctx, "Successfully deleted network devices resource", map[string]interface{}{
+		"network_id": data.NetworkId.ValueString(),
+	})
+}
+
+func handleError(ctx context.Context, err error, httpResp *http.Response, message string, resp *resource.CreateResponse) {
+	resp.Diagnostics.AddError(
+		message,
+		fmt.Sprintf("Could not perform operation, unexpected error: %s", err),
+	)
+	if httpResp != nil {
+		var responseBody string
+		if httpResp.Body != nil {
+			bodyBytes, readErr := io.ReadAll(httpResp.Body)
+			if readErr == nil {
+				responseBody = string(bodyBytes)
+			}
+		}
+		tflog.Error(ctx, "API Request failed", map[string]interface{}{
+			"error":          err.Error(),
+			"httpStatusCode": httpResp.StatusCode,
+			"responseBody":   responseBody,
+		})
+		resp.Diagnostics.AddError(
+			"API Request failed",
+			fmt.Sprintf("HTTP Response: %v\nResponse Body: %s", httpResp, responseBody),
+		)
+	}
+}
+
+// Extracts strings from a given attribute set
+func extractSerials(serials types.Set) []string {
+	var results []string
+	for _, serial := range serials.Elements() {
+		results = append(results, strings.Trim(serial.String(), "\""))
+	}
+
+	return results
+}
+
+// difference returns elements in 'a' that are not in 'b'.
+func difference(a, b []string) []string {
+	bMap := make(map[string]bool)
+	for _, item := range b {
+		bMap[item] = true
+	}
+
+	var diff []string
+	for _, item := range a {
+		if _, found := bMap[item]; !found {
+			diff = append(diff, item)
+		}
+	}
+	return diff
+}
+
+// Handles claiming or removing devices based on the provided action
+func manageDeviceClaims(ctx context.Context, client *openApiClient.APIClient, networkID string, serials []string, isAdd bool, resp *resource.UpdateResponse) error {
+	var httpResp *http.Response
+	var err error
+
+	if isAdd {
+		claimRequest := *openApiClient.NewClaimNetworkDevicesRequest(serials)
+		httpResp, err = client.NetworksApi.ClaimNetworkDevices(ctx, networkID).ClaimNetworkDevicesRequest(claimRequest).Execute()
+	} else {
+		for _, serial := range serials {
+			removeRequest := *openApiClient.NewRemoveNetworkDevicesRequest(serial)
+			httpResp, err = client.NetworksApi.RemoveNetworkDevices(ctx, networkID).RemoveNetworkDevicesRequest(removeRequest).Execute()
+		}
+	}
+
+	if err != nil {
+		resp.Diagnostics.AddError("Error managing devices", err.Error())
+		if httpResp != nil {
+			resp.Diagnostics.AddError("HTTP Response", tools.HttpDiagnostics(httpResp))
+		}
+		return fmt.Errorf("failed to manage devices: %w", err)
+	}
+
+	expectedStatusCode := 200
+	if !isAdd {
+		expectedStatusCode = 204
+	}
+
+	if httpResp.StatusCode != expectedStatusCode {
+		resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("Expected status code %d but got %v", expectedStatusCode, httpResp.StatusCode))
+		return fmt.Errorf("unexpected status code: %d", httpResp.StatusCode)
+	}
+	return nil
+}
+
+// Handles the API call to remove devices from a network
+func removeDevices(ctx context.Context, client *openApiClient.APIClient, networkID string, serials []string, resp *resource.DeleteResponse) error {
+
+	for _, serial := range serials {
+		removeNetworkDevices := *openApiClient.NewRemoveNetworkDevicesRequest(serial)
+		httpResp, err := client.NetworksApi.RemoveNetworkDevices(ctx, networkID).RemoveNetworkDevicesRequest(removeNetworkDevices).Execute()
+		if err != nil {
+			resp.Diagnostics.AddError("Error removing devices", err.Error())
+			return fmt.Errorf("failed to remove devices: %w", err)
+		}
+
+		if httpResp.StatusCode != 204 {
+			resp.Diagnostics.AddError("Unexpected HTTP Response Status Code", fmt.Sprintf("Expected 204 but received %d", httpResp.StatusCode))
+			return fmt.Errorf("unexpected status code: %d", httpResp.StatusCode)
+		}
+
+	}
+	return nil
+}
+
+func mergeSerials(planSerials []string, serialsToAdd []string) []string {
+	// Create a map to keep track of the existing serials in planSerials
+	serialMap := make(map[string]bool)
+	for _, serial := range planSerials {
+		serialMap[serial] = true
+	}
+
+	// Add only the unique serials from serialsToAdd
+	for _, serial := range serialsToAdd {
+		if !serialMap[serial] {
+			planSerials = append(planSerials, serial)
+			serialMap[serial] = true
+		}
+	}
+
+	return planSerials
 }
 
 func (r *NetworksDevicesClaimResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
@@ -277,18 +451,4 @@ func (r *NetworksDevicesClaimResource) ImportState(ctx context.Context, req reso
 	if resp.Diagnostics.HasError() {
 		return
 	}
-}
-
-func difference(a, b []string) []string {
-	mb := make(map[string]struct{}, len(b))
-	for _, x := range b {
-		mb[x] = struct{}{}
-	}
-	var diff []string
-	for _, x := range a {
-		if _, found := mb[x]; !found {
-			diff = append(diff, x)
-		}
-	}
-	return diff
 }
--- internal/provider/networks_devices_claim_resource_test.go
+++ internal/provider/networks_devices_claim_resource_test.go
@@ -2,7 +2,11 @@ package provider
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"os"
+	"reflect"
+	"sort"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -11,6 +15,19 @@ import (
 // TestAccNetworksDevicesClaimResource function is used to test the CRUD operations of the Terraform resource you are developing.
 // It runs the test cases in order to create, read, update, and delete the resource and checks the state at each step.
 func TestAccNetworksDevicesClaimResource(t *testing.T) {
+
+	claimDevices := []string{
+		os.Getenv("TF_ACC_MERAKI_MR_SERIAL"),
+		os.Getenv("TF_ACC_MERAKI_MS_SERIAL"),
+		os.Getenv("TF_ACC_MERAKI_MX_SERIAL"),
+	}
+
+	updateDevices := []string{
+		os.Getenv("TF_ACC_MERAKI_MR_SERIAL"),
+		os.Getenv("TF_ACC_MERAKI_MG_SERIAL"),
+		os.Getenv("TF_ACC_MERAKI_MS_SERIAL"),
+		os.Getenv("TF_ACC_MERAKI_MX_SERIAL"),
+	}
 
 	// The resource.Test function is used to run the test cases.
 	resource.Test(t, resource.TestCase{
@@ -21,38 +38,35 @@ func TestAccNetworksDevicesClaimResource(t *testing.T) {
 		// Steps is a slice of TestStep where each TestStep represents a test case.
 		Steps: []resource.TestStep{
 
-			// Claim a Device by Serial into the Organization
-			/*
-				{
-						Config: testAccNetworksDevicesClaimResourceConfigClaimOrgSerial(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"),
-							os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
-						Check: resource.ComposeAggregateTestCheckFunc(
-							resource.TestCheckResourceAttr("meraki_organizations_claim.test_serial", "id", "example-id"),
-							resource.TestCheckResourceAttr("meraki_organizations_claim.test_serial", "serials.#", "1"),
-							resource.TestCheckResourceAttr("meraki_organizations_claim.test_serial", "serials.0", os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
-						),
-					},
-			*/
-
-			// Create and Read a Network. If a network with the same name already exists this will not create.
+			// create network
 			{
-				Config: testAccNetworksDevicesClaimResourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID")),
+				Config: testAccDevicesClaimResourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID")),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_network.test", "name", "test_acc_network_devices_claim"),
+					resource.TestCheckResourceAttr("meraki_network.test", "name", "test_acc_network_claim_device"),
 					resource.TestCheckResourceAttr("meraki_network.test", "timezone", "America/Los_Angeles"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.#", "1"),
 					resource.TestCheckResourceAttr("meraki_network.test", "tags.0", "tag1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "appliance"),
+					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "3"),
 					resource.TestCheckResourceAttr("meraki_network.test", "notes", "Additional description of the network"),
 				),
 			},
 
-			// Claim and Read NetworksDevicesClaim
+			// Claim Device serials
 			{
-				Config: testAccNetworksDevicesClaimResourceConfigCreate(os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
+				Config: testAccDevicesClaimResourceConfigDeviceClaimWithSerials(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), claimDevices),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_networks_devices_claim.test", "serials.0", os.Getenv("TF_ACC_MERAKI_MX_SERIAL")),
+					resource.TestCheckResourceAttr("meraki_networks_devices_claim.test", "serials.#", "3"),
+					testCheckSerialsUnordered("meraki_networks_devices_claim.test", claimDevices),
+				),
+			},
+
+			// Update and Claim additional Device serial
+			{
+				Config: testAccDevicesClaimResourceConfigDeviceClaimWithSerials(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), updateDevices),
+				Check: resource.ComposeAggregateTestCheckFunc(
+
+					resource.TestCheckResourceAttr("meraki_networks_devices_claim.test", "serials.#", "4"),
+					testCheckSerialsUnordered("meraki_networks_devices_claim.test", updateDevices),
 				),
 			},
 
@@ -66,52 +80,81 @@ func TestAccNetworksDevicesClaimResource(t *testing.T) {
 	})
 }
 
-/*
-func testAccNetworksDevicesClaimResourceConfigClaimOrgSerial(orgId, serial string) string {
-	result := fmt.Sprintf(`
+// Custom test check function to verify serials presence regardless of order
+func testCheckSerialsUnordered(resourceName string, expectedSerials []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", resourceName)
+		}
 
-	resource "meraki_organizations_claim" "test_serial" {
-		organization_id = %s
-		orders = []
-		licences = []
-		serials = ["%s"]
+		serialCountAttr, ok := rs.Primary.Attributes["serials.#"]
+		if !ok {
+			return fmt.Errorf("serials attribute not found")
+		}
+
+		serialCount, err := strconv.Atoi(serialCountAttr)
+		if err != nil {
+			return fmt.Errorf("failed to parse serial count: %s", err)
+		}
+
+		if serialCount != len(expectedSerials) {
+			return fmt.Errorf("expected %d serials but found %d", len(expectedSerials), serialCount)
+		}
+
+		actualSerials := make([]string, serialCount)
+		for i := 0; i < serialCount; i++ {
+			actualSerials[i] = rs.Primary.Attributes[fmt.Sprintf("serials.%d", i)]
+		}
+
+		sort.Strings(actualSerials)
+		sort.Strings(expectedSerials)
+
+		if !reflect.DeepEqual(expectedSerials, actualSerials) {
+			return fmt.Errorf("expected serials %v but found %v", expectedSerials, actualSerials)
+		}
+
+		return nil
 	}
-`, orgId, serial)
-	return result
 }
-*/
 
-// testAccNetworksDevicesClaimResourceConfigCreateNetwork is a constant string that defines the configuration for creating a network resource in your tests.
-// It depends on the organization resource.
-func testAccNetworksDevicesClaimResourceConfigCreateNetwork(orgId string) string {
-	result := fmt.Sprintf(`
+func testAccDevicesClaimResourceConfigCreateNetwork(orgId string) string {
+	return fmt.Sprintf(`
 resource "meraki_network" "test" {
 	organization_id = "%s"
-	product_types = ["appliance"]
+	name = "test_acc_network_claim_device"
+	product_types = ["wireless", "switch", "appliance"]
 	tags = ["tag1"]
-	name = "test_acc_network_devices_claim"
 	timezone = "America/Los_Angeles"
 	notes = "Additional description of the network"
 }
 `, orgId)
-	return result
 }
 
-// testAccNetworksDevicesClaimResourceConfigCreate is a constant string that defines the configuration for creating and reading a networks_devices_claim resource in your tests.
-// It depends on both the organization and network resources.
-func testAccNetworksDevicesClaimResourceConfigCreate(serial string) string {
-	result := fmt.Sprintf(`
+func testAccDevicesClaimResourceConfigDeviceClaimWithSerials(orgId string, serials []string) string {
+	serialsFormatted := ""
+	for _, serial := range serials {
+		serialsFormatted += fmt.Sprintf("\"%s\", ", serial)
+	}
+
+	// Remove the trailing comma and space
+	serialsFormatted = serialsFormatted[:len(serialsFormatted)-2]
+
+	return fmt.Sprintf(`
 resource "meraki_network" "test" {
-        product_types = ["appliance"]
-}    
+	organization_id = "%s"
+ 	name = "test_acc_network_claim_device"
+	product_types = ["wireless", "switch", "appliance", "cellularGateway"]
+	tags = ["tag1"]
+	timezone = "America/Los_Angeles"
+	notes = "Additional description of the network"
+}
 
 resource "meraki_networks_devices_claim" "test" {
-    depends_on = [resource.meraki_network.test]
+    depends_on = ["resource.meraki_network.test"]
     network_id = resource.meraki_network.test.network_id
-    serials = [
-      "%s"
-  ]
-}	
-`, serial)
-	return result
+    serials = [%s]
+}
+
+`, orgId, serialsFormatted)
 }
