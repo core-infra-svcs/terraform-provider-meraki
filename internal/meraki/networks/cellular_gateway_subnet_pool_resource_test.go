a669cb0b21b49fa8e552cc1c144bf342d2d5a7bf
Riley Jacobson <jrileyjacobson@gmail.com>
2024-11-15 11:44:36 -0800

    Updated cellular gateway subnet pool resource tests

--- internal/meraki/networks/cellular_gateway_subnet_pool_resource_test.go
+++ internal/meraki/networks/cellular_gateway_subnet_pool_resource_test.go
@@ -3,6 +3,7 @@ package networks_test
 import (
 	"fmt"
 	"github.com/core-infra-svcs/terraform-provider-meraki/internal/provider"
+	"github.com/core-infra-svcs/terraform-provider-meraki/internal/utils"
 	"os"
 	"testing"
 
@@ -22,75 +23,52 @@ func TestAccNetworksCellularGatewaySubnetPoolResource(t *testing.T) {
 		// Steps is a slice of TestStep where each TestStep represents a test case.
 		Steps: []resource.TestStep{
 
-			// Create and Read a Network.
+			// Create and Read Network
 			{
-				Config: testAccNetworksCellularGatewaySubnetPoolResourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID")),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_network.test", "name", "test_acc_networks_cellular_gateway_subnet_pool"),
-					resource.TestCheckResourceAttr("meraki_network.test", "timezone", "America/Los_Angeles"),
-					resource.TestCheckResourceAttr("meraki_network.test", "tags.#", "1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "tags.0", "tag1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "4"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "appliance"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.1", "cellularGateway"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.2", "switch"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.3", "wireless"),
-					resource.TestCheckResourceAttr("meraki_network.test", "notes", "Additional description of the network"),
-				),
+				Config: utils.CreateNetworkOrgIdConfig(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), "test_acc_networks_cellular_gateway_subnet_pool"),
+				Check:  utils.NetworkOrgIdTestChecks("test_acc_networks_cellular_gateway_subnet_pool"),
 			},
 
 			// TODO: Update and Read NetworksCellularGatewaySubnetPool
 			{
-				Config: testAccNetworksCellularGatewaySubnetPoolResourceConfigUpdate,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					//resource.TestCheckResourceAttr("meraki_networks_cellular_gateway_subnet_pool.test", "id", "example-id"),
-					resource.TestCheckResourceAttr("meraki_networks_cellular_gateway_subnet_pool.test", "deployment_mode", "routed"),
-					resource.TestCheckResourceAttr("meraki_networks_cellular_gateway_subnet_pool.test", "cidr", "192.168.0.0/22"),
-					resource.TestCheckResourceAttr("meraki_networks_cellular_gateway_subnet_pool.test", "mask", "24"),
-				),
+				Config: NetworksCellularGatewaySubnetPoolResourceConfigCreate(),
+				Check:  NetworksCellularGatewaySubnetPoolResourceConfigCreateChecks(),
 			},
+
+			//// ImportState test case.
+			//{
+			//	ResourceName:      "meraki_networks_cellular_gateway_subnet_pool.test",
+			//	ImportState:       true,
+			//	ImportStateVerify: false,
+			//	ImportStateId:     "1234567890",
+			//},
 		},
-		// ImportState test case.
-		/*
-		   {
-		       ResourceName:      "meraki_networks_cellular_gateway_subnet_pool.test",
-		       ImportState:       true,
-		       ImportStateVerify: false,
-		       ImportStateId:     "1234567890",
-		   },
-		*/
-
 	})
-}
-
-// testAccNetworksCellularGatewaySubnetPoolResourceConfigCreateNetwork is a constant string that defines the configuration for creating a network resource in your tests.
-// It depends on the organization resource.
-func testAccNetworksCellularGatewaySubnetPoolResourceConfigCreateNetwork(orgId string) string {
-	result := fmt.Sprintf(`
-
-resource "meraki_network" "test" {
-	organization_id = %s
-	product_types = ["appliance", "switch", "wireless", "cellularGateway"]
-	tags = ["tag1"]
-	name = "test_acc_networks_cellular_gateway_subnet_pool"
-	timezone = "America/Los_Angeles"
-	notes = "Additional description of the network"
-}
-`, orgId)
-	return result
 }
 
 // testAccNetworksCellularGatewaySubnetPoolResourceConfigUpdate is a constant string that defines the configuration for updating a networks_cellularGateway_subnetPool resource in your tests.
 // It depends on both the organization and network resources.
-const testAccNetworksCellularGatewaySubnetPoolResourceConfigUpdate = `
-resource "meraki_network" "test" {
-	product_types = ["appliance", "switch", "wireless", "cellularGateway"]
+func NetworksCellularGatewaySubnetPoolResourceConfigCreate() string {
+	return fmt.Sprintf(`
+	%s
+
+	resource "meraki_networks_cellular_gateway_subnet_pool" "test" {
+		depends_on = [resource.meraki_network.test]
+		id = resource.meraki_network.test.network_id
+		cidr = "192.168.0.0/22"
+		mask = 24    
+	}
+	`,
+		utils.CreateNetworkOrgIdConfig(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), "test_acc_networks_cellular_gateway_subnet_pool"),
+	)
 }
 
-resource "meraki_networks_cellular_gateway_subnet_pool" "test" {
-	depends_on = [resource.meraki_network.test]
-  	network_id = resource.meraki_network.test.network_id
-    cidr = "192.168.0.0/22"
-    mask = 24    
+// NetworksCellularGatewaySubnetPoolResourceConfigCreateChecks returns the aggregated test check functions for the cellular gateway subnet pool resource
+func NetworksCellularGatewaySubnetPoolResourceConfigCreateChecks() resource.TestCheckFunc {
+	expectedAttrs := map[string]string{
+		"deployment_mode": "routed",
+		"cidr":            "192.168.0.0/22",
+		"mask":            "24",
+	}
+	return utils.ResourceTestCheck("meraki_networks_cellular_gateway_subnet_pool.test", expectedAttrs)
 }
-`
