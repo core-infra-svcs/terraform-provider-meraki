7c1f296e7539a3d552de5ab6f6df5bbea691e290
Riley Jacobson <jrileyjacobson@gmail.com>
2024-11-26 14:03:32 -0800

    Update qos rules tests

--- internal/meraki/networks/switch/qos_rule_resource_test.go
+++ internal/meraki/networks/switch/qos_rule_resource_test.go
@@ -3,6 +3,7 @@ package _switch_test
 import (
 	"fmt"
 	"github.com/core-infra-svcs/terraform-provider-meraki/internal/provider"
+	"github.com/core-infra-svcs/terraform-provider-meraki/internal/utils"
 	"os"
 	"testing"
 
@@ -17,44 +18,22 @@ func TestAccNetworkSwitchQosRuleResource(t *testing.T) {
 		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 
-			// Create and Read Network.
+			// Create and Read Network
 			{
-				Config: testAccNetworkSwitchQosRuleResourceConfigCreateNetwork(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID")),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_network.test", "name", "test_acc_network_switch_qos_rule"),
-					resource.TestCheckResourceAttr("meraki_network.test", "timezone", "America/Los_Angeles"),
-					resource.TestCheckResourceAttr("meraki_network.test", "tags.#", "1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "tags.0", "tag1"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.#", "3"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.0", "appliance"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.1", "switch"),
-					resource.TestCheckResourceAttr("meraki_network.test", "product_types.2", "wireless"),
-					resource.TestCheckResourceAttr("meraki_network.test", "notes", "Additional description of the network"),
-				),
+				Config: utils.CreateNetworkOrgIdConfig(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), "test_acc_network_switch_qos_rule"),
+				Check:  utils.NetworkOrgIdTestChecks("test_acc_network_switch_qos_rule"),
 			},
 
 			// Create and Read Networks Switch Qos Rules.
 			{
-				Config: testAccNetworkSwitchQosRuleResourceConfigCreateNetworkSwitchQosRule,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "vlan", "100"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "dst_port", "3000"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "src_port", "2000"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "dscp", "0"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "protocol", "TCP"),
-				),
+				Config: NetworkSwitchQosRuleResourceConfigCreate(),
+				Check:  NetworkSwitchQosRuleResourceConfigCreateChecks(),
 			},
 
 			// Update Networks Switch Qos Rules.
 			{
-				Config: testAccNetworkSwitchQosRuleResourceConfigUpdateNetworkSwitchQosRule,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "vlan", "101"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "dst_port", "4000"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "src_port", "3000"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "dscp", "0"),
-					resource.TestCheckResourceAttr("meraki_networks_switch_qos_rule.test", "protocol", "UDP"),
-				),
+				Config: NetworkSwitchQosRuleResourceConfigUpdate(),
+				Check:  NetworkSwitchQosRuleResourceConfigUpdateChecks(),
 			},
 
 			// Import testing
@@ -67,24 +46,9 @@ func TestAccNetworkSwitchQosRuleResource(t *testing.T) {
 	})
 }
 
-func testAccNetworkSwitchQosRuleResourceConfigCreateNetwork(orgId string) string {
-	result := fmt.Sprintf(`
-resource "meraki_network" "test" {
-    organization_id = %s
-    product_types = ["appliance", "switch", "wireless"]
-    tags = ["tag1"]
-    name = "test_acc_network_switch_qos_rule"
-    timezone = "America/Los_Angeles"
-    notes = "Additional description of the network"
-}
-`, orgId)
-	return result
-}
-
-const testAccNetworkSwitchQosRuleResourceConfigCreateNetworkSwitchQosRule = `
-resource "meraki_network" "test" {
-    product_types = ["appliance", "switch", "wireless"]
-}
+func NetworkSwitchQosRuleResourceConfigCreate() string {
+	return fmt.Sprintf(`
+	%s
 resource "meraki_networks_switch_qos_rule" "test" {
     depends_on = [resource.meraki_network.test]
     network_id = resource.meraki_network.test.network_id
@@ -94,12 +58,27 @@ resource "meraki_networks_switch_qos_rule" "test" {
     dst_port = 3000
     dscp = 0
 }
-`
-
-const testAccNetworkSwitchQosRuleResourceConfigUpdateNetworkSwitchQosRule = `
-resource "meraki_network" "test" {
-    product_types = ["appliance", "switch", "wireless"]
+	
+	`,
+		utils.CreateNetworkOrgIdConfig(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), "test_acc_network_switch_qos_rule"),
+	)
 }
+
+// NetworkSwitchQosRuleResourceConfigCreateChecks returns the test check functions for NetworkSwitchQosRuleResourceConfigCreate
+func NetworkSwitchQosRuleResourceConfigCreateChecks() resource.TestCheckFunc {
+	expectedAttrs := map[string]string{
+		"vlan":     "100",
+		"dst_port": "3000",
+		"src_port": "2000",
+		"dscp":     "0",
+		"protocol": "TCP",
+	}
+	return utils.ResourceTestCheck("meraki_networks_switch_qos_rule.test", expectedAttrs)
+}
+
+func NetworkSwitchQosRuleResourceConfigUpdate() string {
+	return fmt.Sprintf(`
+	%s
 resource "meraki_networks_switch_qos_rule" "test" {
     depends_on = [resource.meraki_network.test]
     network_id = resource.meraki_network.test.network_id
@@ -109,4 +88,19 @@ resource "meraki_networks_switch_qos_rule" "test" {
     dst_port = 4000
     dscp = 0
 }
-`
+	`,
+		utils.CreateNetworkOrgIdConfig(os.Getenv("TF_ACC_MERAKI_ORGANIZATION_ID"), "test_acc_network_switch_qos_rule"),
+	)
+}
+
+// NetworkSwitchQosRuleResourceConfigUpdateChecks returns the test check functions for NetworkSwitchQosRuleResourceConfigUpdate
+func NetworkSwitchQosRuleResourceConfigUpdateChecks() resource.TestCheckFunc {
+	expectedAttrs := map[string]string{
+		"vlan":     "101",
+		"dst_port": "4000",
+		"src_port": "3000",
+		"dscp":     "0",
+		"protocol": "UDP",
+	}
+	return utils.ResourceTestCheck("meraki_networks_switch_qos_rule.test", expectedAttrs)
+}
